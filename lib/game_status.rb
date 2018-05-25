df7bd31c9ee3ca29a04a16cacae253619c4a906e
Valerie Moore <periwinkle17@outlook.com>
2018-05-25 16:56:22 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,9 @@ WIN_COMBINATIONS = [
   
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0] # 0
-    win_index_2 = win_combination[1] # 1
-    win_index_3 = win_combination[2] # 2
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
     
     # load the value of the board at each win_index
     position_1 = board[win_index_1] # board[0]
